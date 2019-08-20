ec8972e9421150082bf986615246ac9b5c9ca111
saraang <saraangsk@gmail.com>
2019-08-20 13:00:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-web-apps
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-web-apps)
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+import { NgxLoadersModule } from '@ngx-lite/loaders';
+import { NgHttpLoaderModule } from 'ng-http-loader';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
